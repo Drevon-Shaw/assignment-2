8a308c51740fa9803f97e8e337059aa3009bc20a
Drevon <shawdrevon@gmail.com>
2023-10-16 17:40:47 -0400

    Simulate monthly production and sale for a specified number of months

--- Assignment 2.py
+++ Assignment 2.py
@@ -8,3 +8,15 @@ class product:
         self.manufacture_cost = manufacture_cost
         self.stock_level = stock_level
         self.esitmated_units_manufactured = estimated_units_manufactured
+
+# define the monthley sales 
+
+    def simiulate_monthly_sales(self, months): 
+        for _ in range(months):
+            units_manufactured = self.estimated_units_manufactured
+            units_sold = random.randint(units_manufactured - 10, units_manufactured + 10)
+            self.stock_level += units_manufactured - units_sold
+            self.monthly_stock.append(self.stock_level)
+            profit_or_loss = (units_sold * self.sale_price) - (units_manufactured * self.manufacture_cost)
+            self.monthly_profit.append(profit_or_loss)
+
