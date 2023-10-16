32ba2d0617137582890a92ff031adaedb7642a24
Drevon <shawdrevon@gmail.com>
2023-10-16 17:35:52 -0400

    Created class product and initialized product

---
+++ Assignment 2.py
@@ -0,0 +1,10 @@
+# 
+
+class product:
+    def __init__(self, code, name, sale_price, manufacture_cost, stock_level, estimated_units_manufactured)
+        self.code = code
+        self.name = name
+        self.sale_price = sale_price
+        self.manufacture_cost = manufacture_cost
+        self.stock_level = stock_level
+        self.esitmated_units_manufactured = estimated_units_manufactured
