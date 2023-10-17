036acab08dd6363e6ebbe0e73a0908d8dc038ac1
Drevon <shawdrevon@gmail.com>
2023-10-16 20:20:08 -0400

    create a simulation for monthly sales, create a while loop until month is greater for the total amount of months

--- Assignment 2/assignment2.1.py
+++ Assignment 2/assignment2.1.py
@@ -3,4 +3,12 @@
 
 class Product:
     def __init__(self, code, name, sale_price, manufacture_cost, stock_level, estimated_units_manufactured):
-        
+        self.code = code
+        self.name = name
+        self.sale_price = sale_price
+        self.manufacture_cost = manufacture_cost
+        self.stock_level = stock_level
+        self.estimated_units_manufactured = estimated_units_manufactured
+        self.monthly_stock = [stock_level]
+        self.monthly_profit = []
+
